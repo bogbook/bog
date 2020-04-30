908213fe62803592944bb45f19b22af699be0723
Ev Bogue <ev@evbogue.com>
2020-04-30 18:14:29 -0500

    ok, this seems to work, but not sure if I need to so much close and nexts

--- gossip.js
+++ gossip.js
@@ -1,12 +1,124 @@
-
-
 function getfeed (feed, pub, keys) {
-  box(feed, pub.pubkey, keys).then(boxed => {
-    var es = new EventSource(keys.publicKey + boxed) 
-    es.addEventListener('message', (e) => {
-      console.log(e.data)
-      es.close()
+  function reqfeed (seq, log) {
+    var memo = feed + seq
+    console.log('CLIENT:' + memo)
+    box(memo, pub.pubkey, keys).then(boxed => {
+      var es = new EventSource(keys.publicKey + boxed) 
+      es.addEventListener('message', (e) => {
+        var pubkey = e.data.substring(0, 45)
+        var reqboxed = e.data.substring(45)
+
+        unbox(reqboxed, pubkey, keys).then(unboxed => {
+          if (unboxed[0] === '@') {
+            console.log('SERVER:' + unboxed)
+            var reqpub = unboxed.substring(0, 45)
+            var serverseq = unboxed.substring(45)
+            if (serverseq < seq) {
+              // send more feed to the server 
+              console.log('SEND MORE FEED TO SERVER')
+              var endrange = log.length - serverseq - 25
+              if (endrange < 0) {
+                endrange = log.length - serverseq - 1
+              }
+              var baserange = log.length - serverseq
+              var diff = JSON.stringify(log.slice(endrange,baserange))
+              box(diff, pub.pubkey, keys).then(nextboxed => {
+                var newes = new EventSource(keys.publicKey + nextboxed)
+                //newes.close()
+              })
+            }
+            else {
+              es.close()
+            }
+          } 
+
+          if (unboxed[0] === '[') {
+            console.log('RECEIVED MORE FEED FROM SERVER')
+            var newfeed = JSON.parse(unboxed)
+            console.log(newfeed)
+
+            open(newfeed[0]).then(msg => {
+              readBog(msg.author).then(feed => {
+                if (!feed[0]) {
+                  writeBog(msg.author, newfeed)
+                  for (var i = newfeed.length -1; i >= 0; --i) {
+                    open(newfeed[i]).then(opened => {
+                      console.log('new msg from ' + opened.author)
+                      masterlog.unshift(opened)
+                      var src = window.location.hash.substring(1)
+                      if ((src === msg.author) || (src === '')) {
+                        var scroller = document.getElementById('scroller')
+                        scroller.insertBefore(render(opened, keys), scroller.childNodes[1])
+                      }
+
+                      if (opened.seq === newfeed.length) {
+                        writeBog('log', masterlog)
+                        es.close()
+                      }
+                    })
+                  }
+                }
+
+                if (feed[0]) {
+                  open(feed[0]).then(lastmsg => {
+                    if (newfeed.length + lastmsg.seq === msg.seq) {
+                      var newlog = newfeed.concat(feed)
+                      writeBog(msg.author, newlog)
+                      for (var i = newfeed.length -1; i >= 0; --i) {
+                        open(newfeed[i]).then(opened => {
+                          console.log('new msg from ' + opened.author)
+                          masterlog.unshift(opened)
+                          var src = window.location.hash.substring(1)
+                          if ((src === msg.author) || (src === '')) {
+                            var scroller = document.getElementById('scroller')
+                            scroller.insertBefore(render(opened, keys), scroller.childNodes[1])
+                          }
+
+                          if (newfeed.length + lastmsg.seq === opened.seq) {
+                            writeBog('log', masterlog)
+                            es.close()
+                          }
+                        })
+                      }
+
+                    }
+                  })
+                }
+              })
+              for (var i = newfeed.length -1; i >= 0; --i) {
+                open(newfeed[i]).then(opened => {
+                  console.log('new msg from ' + opened.author)
+                  masterlog.unshift(opened)
+                  var src = window.location.hash.substring(1)
+                  if ((src === msg.author) || (src === '')) {
+                    var scroller = document.getElementById('scroller')
+                    scroller.insertBefore(render(opened, keys), scroller.childNodes[1])
+                  }
+
+                  if (opened.seq === newfeed.length) {
+                    writeBog('log', masterlog)
+                    es.close()
+                  }
+                })
+              }
+            })
+          } else {es.close()}
+        })
+        es.close()
+      })
     })
+  }
+
+  readBog(feed).then(log => {
+    if (!log[0]) {
+      var seq = 0
+      reqfeed(seq, log)
+    } else {
+      open(log[0]).then(opened => {
+        var seq = opened.seq
+        reqfeed(seq, log)
+      })
+    } 
   })
 }
 
--- server.js
+++ server.js
@@ -27,16 +27,126 @@ bog.keys().then(keys => {
       var pubkey = req.url.substring(1, 46)
       var boxed = req.url.substring(46)
       bog.unbox(boxed, pubkey, keys).then(unboxed => {
-        console.log(unboxed)
+        if (unboxed[0] === '@') {
+          console.log('CLIENT:' + unboxed)
+          var feed = unboxed.substring(0, 45)
+          var clientseq = unboxed.substring(45)
+          bog.readBog(feed).then(log => {
+            if (!log[0]) {
+              var seq = 0
+              var memo = feed + seq
+              console.log('SERVER:' + memo)
+              if (clientseq > seq) {
+                bog.box(memo, pubkey, keys).then(nextboxed => {
+                  res.write(Date.now() + '\ndata:' + keys.publicKey + nextboxed + '\n\n')
+                  next()
+                })
+              } 
+              if (seq > clientseq) {
+                // send more feed to the client
+                console.log('SEND MORE FEED TO CLIENT')
+                var endrange = log.length - clientseq - 25
+                if (endrange < 0) {
+                  endrange = log.length - clientseq - 1
+                }
+                var baserange = log.length - clientseq
+                var diff = JSON.stringify(log.slice(endrange, baserange))
+                
+                bog.box(diff, pubkey, keys).then(finalboxed => {
+                  res.write(Date.now() + '\ndata:' + keys.publicKey + finalboxed + '\n\n') 
+                  next()
+                })
+                
+              } else { next() } 
+            } else {
+              bog.open(log[0]).then(opened => {
+                var seq = opened.seq
+                var memo = feed + seq
+                console.log('SERVER:' + memo)
+                if (clientseq > seq) {
+                  bog.box(memo, pubkey, keys).then(nextboxed => {
+                    res.write(Date.now() + '\ndata:' + keys.publicKey + nextboxed + '\n\n')
+                    next()
+                  })
+                }
+
+                if (seq > clientseq) {
+                  // send more feed to the client
+                  console.log('SEND MORE FEED TO CLIENT')
+                  var endrange = log.length - clientseq - 25
+                  if (endrange < 0) {
+                    endrange = log.length - clientseq - 1
+                  }
+                  var baserange = log.length - clientseq
+                  var diff = JSON.stringify(log.slice(endrange, baserange))
+                  bog.box(diff, pubkey, keys).then(finalboxed => {
+                    res.write(Date.now() + '\ndata:' + keys.publicKey + finalboxed + '\n\n')
+                    next()
+                  })
+                }
+
+                else {
+                  bog.box('Thank you.', pubkey, keys).then(finalboxed => {
+                    res.write(Date.now() + '\ndata:' + keys.publicKey + finalboxed + '\n\n')
+                    next()
+                  })
+                }
+              })
+            }
+          })
+        }
+
+        if (unboxed[0] === '[') {
+          console.log('RECEIVED MORE FEED FROM THE CLIENT')
+          var newfeed = JSON.parse(unboxed)
+          console.log(newfeed)
+
+          bog.open(newfeed[0]).then(msg => {
+            bog.readBog(msg.author).then(feed => {
+              if (!feed[0]) {
+                bog.writeBog(msg.author, newfeed)
+                for (var i = newfeed.length -1; i >= 0; --i) {
+                  bog.open(newfeed[i]).then(opened => {
+                    console.log('new msg from ' + opened.author)
+                    masterlog.unshift(opened)
+                    if (opened.seq === newfeed.length) {
+                      bog.writeBog('log', masterlog)
+                      next()
+                    }
+                  })
+                }
+              }
+
+              if (feed[0]) {
+                bog.open(feed[0]).then(lastmsg => {
+                  if (newfeed.length + lastmsg.seq === msg.seq) {
+                    var newlog = newfeed.concat(feed)
+                    bog.writeBog(msg.author, newlog)
+                    for (var i = newfeed.length -1; i >= 0; --i) {
+                      bog.open(newfeed[i]).then(opened => {
+                        console.log('new msg from ' + opened.author)
+                        masterlog.unshift(opened)
+                        if (newfeed.length + lastmsg.seq === opened.seq) {
+                          bog.writeBog('log', masterlog)
+                          next()
+                        }
+                      })
+                    }
+                  }
+                })
+              }
+            })
+          })
+        }
       })
-      res.write(Date.now() + '\ndata:' + 'got it\n\n' )
     }
 
-    next()
+    else {
+      next()
+    }
+
   })
 
-  
   app.listen(PORT)
-  
 })
 

d48199580f83557a79494ecc3891227632d7a088
Ev Bogue <ev@evbogue.com>
2020-06-04 13:23:08 -0500

    add hash router to renderer

--- render.js
+++ render.js
@@ -2,13 +2,13 @@ async function render (msg) {
   var message = h('div', {classList: 'message'})
 
   message.appendChild(h('span', {classList: 'right'}, [
-    h('a', {href: msg.raw.substring(0, 44)}, [
+    h('a', {href: '#' + msg.raw.substring(0, 44)}, [
       human(new Date(msg.timestamp))
     ])
   ]))
 
   message.appendChild(h('span', [
-    h('a', {href: msg.author}, [
+    h('a', {href: '#' + msg.author}, [
       msg.author.substring(0, 10)
     ])
   ]))

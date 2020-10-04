6006614e9a3265acabe8b494194d522782726f16
Ev Bogue <ev@evbogue.com>
2020-10-04 10:01:48 -0500

    fix error with default pubkey

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 var config = {
   title: 'Bogbook | A secure and distributed network of blockchain logs (bogs).', // your site title goes here
-  author: 'Q++V5BbvWIg8B+TqtC9ZKFhetruuw+nOgxEqfjlOZI0\=', // the feed you want to sync be default goes here
+  author: 'Q++V5BbvWIg8B+TqtC9ZKFhetruuw+nOgxEqfjlOZI0=', // the feed you want to sync be default goes here
   searchterm: '#bogbook', // the default wording for the search box goes here
 }

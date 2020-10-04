1a5c208765006d0d6ee92f3def70cff4e7964ecc
Ev Bogue <ev@evbogue.com>
2020-10-04 09:34:15 -0500

    move structural css to a new file

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+// this is the config file for the app.js client. Configure your site title, default author to sync, and the default search term here.
+
 var config = {
   title: 'Bogbook | A secure and distributed network of blockchain logs (bogs).', // your site title goes here
   author: 'Q++V5BbvWIg8B+TqtC9ZKFhetruuw+nOgxEqfjlOZI0\=', // the feed you want to sync be default goes here
